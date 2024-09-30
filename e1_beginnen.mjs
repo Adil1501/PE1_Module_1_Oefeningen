e47773ff6760fb2e6624ad57226563e04de5424a
Adil1501 <ben.adil1501@gmail.com>
2024-09-30 13:45:42 +0200

    Update e1_beginnen.mjs - Adil Nette Versie

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -1,5 +1,5 @@
 // Voeg hier je eigen code in
-
-
-
+let name = 'Adil';
+let age = '20';
+let favoriteGame = 'Hearts Of Iron IV';
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
