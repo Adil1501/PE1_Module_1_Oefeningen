a22d645b4a3ac83e4b2c3d5e1dfcff68f308860d
Adil1501 <ben.adil1501@gmail.com>
2024-09-30 14:06:58 +0200

    Update e3_opteller.mjs - Adil

--- e3_opteller.mjs
+++ e3_opteller.mjs
@@ -7,7 +7,10 @@ const userInput = readline.createInterface({input, output});
 
 
 // Voeg hier je eigen code in
+let jeGetal1 = parseFloat(await userInput.question('Geef een eerste getal om de som te berekenen. '));
+let jeGetal2 = parseFloat(await userInput.question('Geef een tweede getal. '));
+let som = jeGetal1 + jeGetal2
 
-
+console.log('De som van de getal ' + jeGetal1 + ' en ' + jeGetal2 + ' is ' + som + '.');
 
 process.exit();
